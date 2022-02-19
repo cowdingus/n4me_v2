283f450cf62b7def0b5b4f056f18462d700e3815
cowdingus <zaki.nadhif45@gmail.com>
2022-02-19 10:47:30 +0700

    Implement masonry grid in note list

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,15 @@
 import "../css/Main.css";
 import NoteCard from "./NoteCard";
 
+import Masonry from "react-masonry-css"
+
 function Main({ notes, editHandler }) {
   return (
     <main className="main-section">
+      <Masonry
+        breakpointCols={4}
+        className="main-section-grid"
+        columnClassName="main-section-grid_column">
       {notes.map((note) =>
         <NoteCard
           key={note.id}
@@ -15,6 +21,7 @@ function Main({ notes, editHandler }) {
           handleClick={editHandler}
         />
       )}
+      </Masonry>
     </main>
   );
 }
